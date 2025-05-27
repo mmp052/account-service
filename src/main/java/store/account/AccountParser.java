753d245a677976acb1dd43dbc7293bfa8dcb8adb
mmp052 <mateusmoreirammp052@gmail.com>
2025-05-27 18:01:15 -0300

    first commit

---
+++ src/main/java/store/account/AccountParser.java
@@ -0,0 +1,26 @@
+package store.account;
+
+import store.account.AccountIn;
+import store.account.AccountOut;
+
+public class AccountParser {
+
+    public static Account to(AccountIn in) {
+        return in == null ? null :
+            Account.builder()
+                .name(in.name())
+                .email(in.email())
+                .password(in.password())
+                .build();
+    }
+
+    public static AccountOut to(Account a) {
+        return a == null ? null :
+            AccountOut.builder()
+                .id(a.id())
+                .name(a.name())
+                .email(a.email())
+                .build();
+    }
+
+}
